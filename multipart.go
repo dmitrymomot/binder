2eb273aed1d56c7ad8ddc56c5af7df0d500f70d7
Dmitry Momot <mail@dmomot.com>
2023-04-19 10:20:53 +0300

    + multipart form binding

--- multipart.go
+++ multipart.go
@@ -1,22 +1,32 @@
 package binder
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
-	"net/url"
 	"reflect"
+	"strconv"
+	"strings"
 )
 
-type FormData struct {
-	Fields url.Values
-	Files  map[string][]*FileData
+// File represents a file that was uploaded via a multipart form.
+type File struct {
+	// FileName stores the name of the file
+	FileName string
+	// FileSize stores the size of the file in bytes
+	FileSize int64
+	// ContentType stores the MIME type of the file
+	ContentType string
+	// Data is a byte slice that holds the contents of the file
+	Data []byte
 }
 
 // BindFormMultipart binds the passed v pointer to the request.
 // It uses the multipart/form-data content type for binding.
 // `v` param should be a pointer to a struct with `form“ tags.
 // Implements the binder.BinderFunc interface.
-func BindFormMultipart1(r *http.Request, v interface{}) error {
+func BindFormMultipart(r *http.Request, v interface{}) error {
 	// Check if the request method is POST, PUT or PATCH
 	if !isPostPutPatch(r) {
 		return fmt.Errorf("%w: %s", ErrInvalidMethod, r.Method)
@@ -25,11 +35,6 @@ func BindFormMultipart1(r *http.Request, v interface{}) error {
 	// Check if request is multipart
 	if !isMultipartFormData(r) {
 		return ErrInvalidContentType
-	}
-
-	// Validate v pointer before decoding query into it
-	if !isPointer(v) {
-		return fmt.Errorf("%w: v must be a pointer to a struct", ErrInvalidInput)
 	}
 
 	// Parse the request body
@@ -42,96 +47,143 @@ func BindFormMultipart1(r *http.Request, v interface{}) error {
 		return ErrEmptyBody
 	}
 
-	// Create form data struct
-	form := FormData{
-		Fields: make(url.Values),
-		Files:  make(map[string][]*FileData),
+	// Get the target value
+	targetVal := reflect.ValueOf(v)
+	if targetVal.Kind() != reflect.Ptr {
+		return ErrTargetMustBeAPointer
 	}
 
-	// Decode the request body into the v pointer
-	if err := formDecoder.Decode(v, r.MultipartForm.Value); err != nil {
-		return fmt.Errorf("%w: %s", ErrDecodeForm, err.Error())
+	// Get the target element
+	targetElem := targetVal.Elem()
+	if targetElem.Kind() != reflect.Struct {
+		return ErrTargetMustBeAStruct
 	}
 
-	// Convert struct to map
-	structMap, err := structToMap(v)
-	if err != nil {
-		return err
-	}
+	// Get the target type
+	targetType := targetElem.Type()
 
-	// Loop through struct fields and add to form data
-	for key, value := range structMap {
-		// Check if field is a file
-		if isFileData(value) {
-			// Get file data from request
-			fileData, err := GetFileData(r, key)
+	// Iterate over the target fields
+	for i := 0; i < targetType.NumField(); i++ {
+		field := targetType.Field(i)
+		tagStr := field.Tag.Get(TagForm)
+		tag := strings.Split(tagStr, ",")[0]
+
+		// Skip if tag is empty or "-"
+		if tag == "" || tag == "-" {
+			continue
+		}
+
+		// Bind form values
+		if formValue := r.FormValue(tag); formValue != "" {
+			fieldValue := targetElem.Field(i)
+			if fieldValue.CanSet() {
+				switch fieldValue.Kind() {
+				case reflect.String:
+					fieldValue.SetString(formValue)
+				case reflect.Complex128, reflect.Complex64:
+					complexValue, err := strconv.ParseComplex(formValue, 64)
+					if err != nil {
+						return err
+					}
+					fieldValue.SetComplex(complexValue)
+				// FIXME: fix mapping of arrays and slices to a struct
+				// case reflect.Array:
+				// 	arrSlice := strings.Split(formValue, ",")
+				// 	arr := reflect.New(fieldValue.Type()).Elem()
+				// 	fmt.Println(arrSlice)
+				// 	for i := 0; i < arr.Len(); i++ {
+				// 		arr.Index(i).Set(reflect.ValueOf(arrSlice[i]))
+				// 	}
+				// 	fieldValue.Set(reflect.ValueOf(arr))
+				// case reflect.Slice:
+				// 	fieldValue.Set(reflect.ValueOf(strings.Split(formValue, ",")))
+				case reflect.Map:
+					var mapValue map[string]interface{}
+					if err := json.Unmarshal([]byte(formValue), &mapValue); err != nil {
+						return err
+					}
+					fieldValue.Set(reflect.ValueOf(mapValue))
+				case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+					intValue, err := strconv.ParseInt(formValue, 10, 64)
+					if err != nil {
+						return err
+					}
+					fieldValue.SetInt(intValue)
+				case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+					uintValue, err := strconv.ParseUint(formValue, 10, 64)
+					if err != nil {
+						return err
+					}
+					fieldValue.SetUint(uintValue)
+				case reflect.Float32, reflect.Float64:
+					floatValue, err := strconv.ParseFloat(formValue, 64)
+					if err != nil {
+						return err
+					}
+					fieldValue.SetFloat(floatValue)
+				case reflect.Bool:
+					boolValue, err := strconv.ParseBool(formValue)
+					if err != nil {
+						return err
+					}
+					fieldValue.SetBool(boolValue)
+				case reflect.Ptr, reflect.Struct:
+					err := json.Unmarshal([]byte(formValue), fieldValue.Addr().Interface())
+					if err != nil {
+						return err
+					}
+				default:
+					return fmt.Errorf("%w: %s", ErrUnsupportedType, fieldValue.Kind())
+				}
+			}
+		}
+
+		// skip if the field is not a binder.File or *binder.File
+		if field.Type.Kind() != reflect.ValueOf(File{}).Kind() &&
+			field.Type.Kind() != reflect.ValueOf(&File{}).Kind() {
+			continue
+		}
+
+		// Bind file data
+		if formFile, header, err := r.FormFile(tag); err == nil {
+			defer formFile.Close()
+			fileData, err := ioutil.ReadAll(formFile)
 			if err != nil {
 				return err
 			}
-			if _, ok := form.Files[key]; !ok {
-				form.Files[key] = make([]*FileData, 0, 1)
+
+			// Get the file mime type
+			mime, err := GetFileMimeType(fileData)
+			if err != nil {
+				return err
 			}
-			form.Files[key] = append(form.Files[key], fileData)
-			continue
-		}
 
-		// Check if field is a slice of files
-		if isSliceOfFileData(value) {
-			// slice of files is not supported
-			continue
-		}
+			// Create a new File struct
+			fileStruct := &File{
+				FileName:    header.Filename,
+				FileSize:    header.Size,
+				ContentType: mime,
+				Data:        fileData,
+			}
 
-		// Add field to form data
-		// form.Fields.Add(key, fmt.Sprintf("%v", value))
-	}
+			// Marshal the File struct to JSON
+			jsonBytes, err := json.Marshal(fileStruct)
+			if err != nil {
+				return err
+			}
 
-	// Convert struct map back to struct
-	if err := mapToStruct(structMap, v); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Populates a struct with form data
-func populate(v interface{}, form FormData) error {
-	// Convert struct to map
-	structMap, err := structToMap(v)
-	if err != nil {
-		return err
-	}
-
-	if len(form.Fields) == 0 && len(form.Files) == 0 {
-		return ErrEmptyRequestData
-	}
-
-	if len(form.Fields) > 0 {
-		// Decode the request body into the v pointer
-		if err := formDecoder.Decode(v, form.Fields); err != nil {
-			return fmt.Errorf("%w: %s", ErrDecodeForm, err.Error())
-		}
-	}
-
-	// Loop through files and set values in struct map
-	for key, value := range form.Files {
-		if f, ok := structMap[key]; ok {
-			field := reflect.ValueOf(f)
-			if field.Kind() == reflect.Slice {
-				if structMap[key] == nil {
-					structMap[key] = make([]FileData, 0, len(value))
+			// Unmarshal the JSON to the target field
+			fieldValue := targetElem.Field(i)
+			if fieldValue.CanSet() {
+				switch fieldValue.Kind() {
+				case reflect.Ptr, reflect.Struct:
+					err = json.Unmarshal(jsonBytes, fieldValue.Addr().Interface())
+					if err != nil {
+						return err
+					}
 				}
-				for _, file := range value {
-					structMap[key] = append(structMap[key].([]FileData), *file)
-				}
-			} else {
-				structMap[key] = *value[0]
 			}
 		}
-	}
-
-	// Convert struct map back to struct
-	if err := mapToStruct(structMap, v); err != nil {
-		return err
 	}
 
 	return nil
--- multipart_test.go
+++ multipart_test.go
@@ -2,131 +2,105 @@ package binder_test
 
 import (
 	"bytes"
-	"errors"
+	"io"
+	"mime/multipart"
 	"net/http"
+	"net/http/httptest"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/dmitrymomot/binder"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestBindFormMultipart1(t *testing.T) {
-	type objPayload struct {
-		FieldString string  `form:"field_string,omitempty"`
-		FieldInt    int     `form:"field_int,omitempty"`
-		FieldUint   uint    `form:"field_uint,omitempty"`
-		FieldBool   bool    `form:"field_bool,omitempty"`
-		FieldFloat  float64 `form:"field_float,omitempty"`
-		// FieldArray  [2]int            `form:"field_array,omitempty"`
-		// FieldSlice  []int             `form:"field_slice,omitempty"`
-		FieldMap map[string]string `form:"field_map,omitempty"`
+type FormData struct {
+	Name   string       `form:"name,omitempty"`
+	Email  string       `form:"email,omitempty"`
+	Age    int          `form:"age,omitempty"`
+	Avatar *binder.File `form:"avatar,omitempty"`
+}
 
-		TestFile *binder.File `form:"test_file,omitempty"`
+func TestBindFormMultipart(t *testing.T) {
+	tests := []struct {
+		name        string
+		contentType string
+		setupForm   func(*multipart.Writer) error
+		expectErr   bool
+	}{
+		{
+			name:        "ValidFormData",
+			contentType: "multipart/form-data",
+			setupForm: func(w *multipart.Writer) error {
+				if err := w.WriteField("name", "John Doe"); err != nil {
+					return err
+				}
+				if err := w.WriteField("email", "john.doe@example.com"); err != nil {
+					return err
+				}
+				if err := w.WriteField("age", "30"); err != nil {
+					return err
+				}
+
+				filePath := "testdata/test.jpg"
+				file, err := os.Open(filePath)
+				if err != nil {
+					return err
+				}
+				defer file.Close()
+
+				part, err := w.CreateFormFile("avatar", filepath.Base(filePath))
+				if err != nil {
+					return err
+				}
+				if _, err := io.Copy(part, file); err != nil {
+					return err
+				}
+
+				return nil
+			},
+			expectErr: false,
+		},
+		{
+			name:        "InvalidContentType",
+			contentType: "application/x-www-form-urlencoded",
+			setupForm:   func(w *multipart.Writer) error { return nil },
+			expectErr:   true,
+		},
 	}
 
-	// Initialize a dummy request payload
-	payload := map[string]interface{}{
-		"field_string": "value",
-		"field_int":    123,
-		"field_uint":   123,
-		"field_bool":   true,
-		"field_float":  123.456,
-		// "field_array":  [2]int{1, 2},
-		// "field_slice":  []int{1, 2},
-		"field_map": map[string]string{"key": "value"},
-		"test_file": nil,
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			multipartWriter := multipart.NewWriter(&buf)
+
+			err := test.setupForm(multipartWriter)
+			require.NoError(t, err)
+
+			err = multipartWriter.Close()
+			require.NoError(t, err)
+
+			req := httptest.NewRequest(http.MethodPost, "/upload", &buf)
+			req.Header.Set("Content-Type", test.contentType+"; boundary="+multipartWriter.Boundary())
+
+			var data FormData
+			err = binder.BindFormMultipart(req, &data)
+
+			if test.expectErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, "John Doe", data.Name)
+				assert.Equal(t, "john.doe@example.com", data.Email)
+				assert.Equal(t, 30, data.Age)
+				if assert.NotNil(t, data.Avatar) {
+					assert.Equal(t, "test.jpg", data.Avatar.FileName)
+					assert.NotEmpty(t, data.Avatar.Data)
+					assert.Equal(t, "image/jpeg", data.Avatar.ContentType)
+					assert.Greater(t, data.Avatar.FileSize, int64(0))
+				}
+			}
+		})
 	}
-
-	t.Run("invalid method", func(t *testing.T) {
-		req, err := newQueryRequest(http.MethodGet, "/path", nil, nil)
-		if err != nil {
-			t.Errorf("unexpected error: %s", err.Error())
-		}
-
-		var obj objPayload
-		if err := binder.BindFormMultipart(req, &obj); !errors.Is(err, binder.ErrInvalidMethod) {
-			t.Errorf("expected error '%s', got '%s'", binder.ErrInvalidMethod, err)
-		}
-	})
-
-	t.Run("invalid content type", func(t *testing.T) {
-		req, err := newJSONRequest(http.MethodPost, "/path", payload, nil)
-		if err != nil {
-			t.Errorf("unexpected error: %s", err.Error())
-		}
-
-		var obj objPayload
-		if err := binder.BindFormMultipart(req, &obj); !errors.Is(err, binder.ErrInvalidContentType) {
-			t.Errorf("expected error '%s', got '%s'", binder.ErrInvalidContentType, err)
-		}
-	})
-
-	t.Run("invalid input", func(t *testing.T) {
-		req, err := newMultipartRequest(http.MethodPost, "/path", payload, nil)
-		if err != nil {
-			t.Errorf("unexpected error: %s", err.Error())
-		}
-
-		var obj objPayload
-		if err := binder.BindFormMultipart(req, obj); !errors.Is(err, binder.ErrInvalidInput) {
-			t.Errorf("expected error '%s', got '%s'", binder.ErrInvalidInput, err)
-		}
-	})
-
-	t.Run("success: file", func(t *testing.T) {
-		// Read the test image file.
-		testImage, err := os.ReadFile("testdata/test.jpg")
-		if err != nil {
-			t.Fatalf("Failed to read test image file: %s", err.Error())
-		}
-		payload["test_file"] = bytes.NewReader(testImage)
-		req, err := newMultipartRequest(http.MethodPost, "/path", payload, nil)
-		if err != nil {
-			t.Errorf("unexpected error: %s", err.Error())
-		}
-
-		var obj objPayload
-		if err := binder.BindFormMultipart(req, &obj); err != nil {
-			t.Errorf("unexpected error: %s", err.Error())
-		}
-
-		if obj.FieldString != "value" {
-			t.Errorf("expected 'value', got '%s'", obj.FieldString)
-		}
-		if obj.FieldInt != 123 {
-			t.Errorf("expected 123, got %d", obj.FieldInt)
-		}
-		if obj.FieldUint != 123 {
-			t.Errorf("expected 123, got %d", obj.FieldUint)
-		}
-		if obj.FieldBool != true {
-			t.Errorf("expected true, got %t", obj.FieldBool)
-		}
-		if obj.FieldFloat != 123.456 {
-			t.Errorf("expected 123.456, got %f", obj.FieldFloat)
-		}
-		// if obj.FieldArray != [2]int{1, 2} {
-		// 	t.Errorf("expected [1, 2], got %v", obj.FieldArray)
-		// }
-		// if obj.FieldSlice == nil || len(obj.FieldSlice) != 2 || obj.FieldSlice[0] != 1 || obj.FieldSlice[1] != 2 {
-		// 	t.Errorf("expected [1, 2], got %v", obj.FieldSlice)
-		// }
-		if obj.FieldMap == nil || len(obj.FieldMap) != 1 || obj.FieldMap["key"] != "value" {
-			t.Errorf("expected {'key': 'value'}, got %v", obj.FieldMap)
-		}
-
-		if obj.TestFile == nil {
-			t.Error("expected non-nil file data")
-		} else {
-			if obj.TestFile.FileName != "test_file" {
-				t.Errorf("expected 'test_file', got '%s'", obj.TestFile.FileName)
-			}
-			if obj.TestFile.FileSize != int64(len(testImage)) {
-				t.Errorf("expected %d, got %d", len(testImage), obj.TestFile.FileSize)
-			}
-			if obj.TestFile.ContentType != "image/jpeg" {
-				t.Errorf("expected 'image/jpeg', got '%s'", obj.TestFile.ContentType)
-			}
-		}
-	})
 }
